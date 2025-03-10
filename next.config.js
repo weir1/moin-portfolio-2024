b4d745b0bb108c9b4d8023279ea4569eb8122dec
Moin <weircarl@gmail.com>
2025-03-10 16:33:35 +0530

    Add Cloudflare Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -11,5 +13,9 @@ const nextConfig = {
     emotion: true,
   },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig; 
